68047e5a8b3337f44fc91341ab8a084e417b8859
Mateus Luciano da Silva <teeusdm@gmail.com>
2025-08-21 22:17:39 -0300

    build: add cross-platform build script for Windows, Linux and macOS

--- build.bat
+++ build.bat
@@ -1,2 +1,27 @@
 @echo off
-go build -ldflags "-H windowsgui -s -w" -o termnia.exe ./cmd/termnia
+setlocal
+
+set APP=termnia
+set SRC=./cmd/termnia
+set BIN=bin
+
+if not exist %BIN% mkdir %BIN%
+
+echo ==== Building Windows (amd64) ====
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags "-H windowsgui -s -w" -o %BIN%\%APP%-windows-amd64.exe %SRC%
+
+echo ==== Building Linux (amd64) ====
+set GOOS=linux
+set GOARCH=amd64
+go build -ldflags "-s -w" -o %BIN%\%APP%-linux-amd64 %SRC%
+
+echo ==== Building macOS (darwin-arm64) ====
+set GOOS=darwin
+set GOARCH=arm64
+go build -ldflags "-s -w" -o %BIN%\%APP%-darwin-arm64 %SRC%
+
+echo ==== Build completed! Binaries are in %BIN% ====
+endlocal
+pause

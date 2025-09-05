e2b4108ffeb61ccc71a2bf6122707d34f912aaeb
Mateus Luciano da Silva <teeusdm@gmail.com>
2025-09-05 20:39:01 -0300

    refactor: simplify main.go by moving terminal startup to core.Run

--- cmd/termnia/termnia.go
+++ cmd/termnia/termnia.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"os"
-	"termnia/internal/config"
-	"termnia/internal/core"
-	"termnia/internal/platform"
-	"termnia/internal/tui"
+	"log"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/mateus-luciano/termnia/internal/core"
 )
 
 func main() {
-	if err := platform.AllocConsole(); err != nil {
-		os.Exit(1)
+	if err := core.Run(); err != nil {
+		log.Fatal(err)
 	}
-	if err := platform.RedirectIO(); err != nil {
-		os.Exit(1)
-	}
-
-	cfg, err := config.Load()
-	if err != nil {
-		os.Exit(1)
-	}
-
-	shell, err := core.NewShellTerminal(cfg.DefaultShell)
-	if err != nil || shell == nil {
-		shell, _ = core.NewShellTerminal(core.DefaultShellForOS())
-	}
-	if err := shell.Start(); err != nil {
-		os.Exit(1)
-	}
-
-	model := tui.NewModel(shell)
-	p := tea.NewProgram(model)
-	_, _ = p.Run()
 }
